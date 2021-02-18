5421bee39d92d45b001dbc46bdbe1f328d642f40
Mahdi959 <57423043+Mahdi959@users.noreply.github.com>
2021-02-18 11:16:30 -0500

    Add files via upload

---
+++ Python 3.py
@@ -0,0 +1,7 @@
+import pywhatkit
+
+a = input("Hi! I'm Learnengo! How are you doing today?\n-")
+a = a.lower()
+t= {"good","well","fine","doing fine","not bad","tat"}
+if a in t:
+    print("happy to hear that")
